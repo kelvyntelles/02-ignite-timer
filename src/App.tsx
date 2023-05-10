44b0b40add36e27974561fcc4770db8061839fa9
kelvyntelles <kelvyn.telles@gmail.com>
2023-05-10 14:49:19 -0300

    configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from "./components/Button";
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button />
+    </ThemeProvider>
   )
 }
 
